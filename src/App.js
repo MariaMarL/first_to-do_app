0459f76746ce330bbe2b935a0d111558f1e93612
Maria del Mar Lamilla <lamilla767@gmail.com>
2022-05-03 00:45:10 -0500

    create button, task and tasks components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import {useState} from 'react';
+
 function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id:1,
+      text:'Doctor appointment',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: true,
+    },
+    {
+      id:2,
+      text:'Meeting at school',
+      day: 'Feb 6th at 1:30 pm',
+      reminder: true,
+    },
+    {
+      id:3,
+      text:'Food shopping',
+      day: 'Feb 5th at 2:30 pm',
+      reminder: false,
+    }
+  ]) 
+
 
   return (
     <div className="container">
-      <Header />
+      <Header/>
+      <Tasks  tasks={tasks}/>
     </div>
   );
 }
